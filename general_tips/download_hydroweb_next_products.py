f487a47eeb95317245fe7cc8439ab248882eb7f7
Lionel Zawadzki <74003469+zawadzl@users.noreply.github.com>
2023-04-03 11:19:43 +0200

    Update download_hydroweb_next_products.py

--- general_tips/download_hydroweb_next_products.py
+++ general_tips/download_hydroweb_next_products.py
@@ -35,12 +35,9 @@ path_out = "/tmp"
 # Define your search criteria. It defines a list of query-arguments dictionnaries.
 # Each query-arguments dictionnary will be used to perform a distinct search, whose results will then be contatenated.
 # - add/remove collections using the `productType` key (one per query-arguments dictionnary)
-# - add time restrictions using the `start` and `end` keys (e.g. "start": "2020-05-01" , "end": "2020-05-10T00:00:00Z",
-#   UTC ISO8601 format)(one per collection/dictionnary)
-# - add spatial restrictions using the "geom" key (e.g. "geom": "POLYGON ((1 43, 2 43, 2 44, 1 44, 1 43))" WKT string,
-#   a bounding-box list [lonmin, latmin, lonmax, latmax] can also be passed )(one per collection/dictionnary)
-# - more query arguments can be used, see
-#   https://eodag.readthedocs.io/en/stable/notebooks/api_user_guide/4_search.html?#Search-parameters
+# - add time restrictions using the `start` and `end` keys (e.g. "start": "2020-05-01" , "end": "2020-05-10T00:00:00Z" with UTC ISO8601 format)(one per collection/dictionnary)
+# - add spatial restrictions using the `geom` key (e.g. "geom": "POLYGON ((1 43, 2 43, 2 44, 1 44, 1 43))" WKT string, a bounding-box list [lonmin, latmin, lonmax, latmax] can also be passed )(one per collection/dictionnary)
+# - more query arguments can be used, see `documentation <https://eodag.readthedocs.io/en/stable/notebooks/api_user_guide/4_search.html?#Search-parameters>`_
 project_query_args = [
     {"productType":"SWOT_L2_HR_RASTER_250M_SAMPLE_V1_2","geom":"POLYGON ((-4.17 43.10,12.70 43.10,12.70 49.64,-4.17 49.64,-4.17 43.10))"}]
 	
